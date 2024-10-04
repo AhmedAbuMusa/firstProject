1663ccd688a102b8b34f0bc120d03acc91f6da06
Ahmed AbuMusa <ahmedabumusa@outlook.com>
2024-10-04 11:28:42 +0300

    test

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,1 +1,14 @@
-pragma solidity version;
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity ^0.8.0;  
+
+contract HelloWorld {
+    string private helloMessage;
+
+    constructor() {
+        helloMessage = "Hello, World!";
+    }
+
+    function getHelloMessage() public view returns (string memory) {
+        return helloMessage;
+    }
+}
